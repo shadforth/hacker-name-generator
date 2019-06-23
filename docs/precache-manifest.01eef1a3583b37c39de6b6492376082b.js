ba53fa7ca1c3bda449a2f45851a8a7b24e281de1
Jacqui Shadforth <jacqui.shadforth@rea-group.com>
2019-06-23 16:31:42 +1000

    Fix compiled file path

--- docs/precache-manifest.01eef1a3583b37c39de6b6492376082b.js
+++ docs/precache-manifest.01eef1a3583b37c39de6b6492376082b.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "034411a4aae9db2c817b53a7c8cb02f8",
-    "url": "/shadforth/hacker-name-generator/index.html"
+    "url": "/hacker-name-generator/index.html"
   },
   {
     "revision": "6534b8df24934a4034ee",
-    "url": "/shadforth/hacker-name-generator/static/css/main.a858a377.chunk.css"
+    "url": "/hacker-name-generator/static/css/main.a858a377.chunk.css"
   },
   {
     "revision": "2315f74ce3c99316133d",
-    "url": "/shadforth/hacker-name-generator/static/js/2.3310f33a.chunk.js"
+    "url": "/hacker-name-generator/static/js/2.3310f33a.chunk.js"
   },
   {
     "revision": "6534b8df24934a4034ee",
-    "url": "/shadforth/hacker-name-generator/static/js/main.4944ce4f.chunk.js"
+    "url": "/hacker-name-generator/static/js/main.4944ce4f.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/shadforth/hacker-name-generator/static/js/runtime~main.a8a9905a.js"
+    "url": "/hacker-name-generator/static/js/runtime~main.a8a9905a.js"
   }
 ]);
